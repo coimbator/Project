f09e554cdd7ab5d61e4f9dd8148a580d5c9e765a
David Luke Issac Prabhu <77135336+coimbator@users.noreply.github.com>
2024-12-13 03:33:12 +0000

    the Project commit

--- Train_model.py
+++ Train_model.py
@@ -4,13 +4,20 @@ from sklearn.metrics import classification_report
 from sklearn.model_selection import train_test_split
 import mlflow
 import joblib
+import argparse
 
+parser = argparse.ArgumentParser()
+parser.add_argument("--trainingdata1", type=str, required=True, help='Dataset for training')
+parser.add_argument("--trainingdata2", type=str, required=True, help='Dataset for training')
+parser.add_argument("--testingdata1", type=str, required=True, help='Dataset for testing')
+parser.add_argument("--testingdata2", type=str, required=True, help='Dataset for testing')
+args = parser.parse_args()
 mlflow.autolog()
 # Load preprocessed data
-X_train= pd.read_csv('data/standardized_train_features.csv').values
-y_train = pd.read_csv('data/train_target.csv').values.ravel()
-X_test= pd.read_csv('data/standardized_test_features.csv').values
-y_test= pd.read_csv('data/test_target.csv').values.ravel()
+X_train= pd.read_csv(args.trainingdata1).values
+y_train = pd.read_csv(args.trainingdata2).values.ravel()
+X_test= pd.read_csv(args.testingdata1).values
+y_test= pd.read_csv(args.testingdata2).values.ravel()
 
 # Initialize the Random Forest classifier
 clf = RandomForestClassifier(n_estimators=100, random_state=42)
